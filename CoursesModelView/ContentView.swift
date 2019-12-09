d91a50f907694c0042053165e91094f236e56f76
Abilio Gambim Parada <abilio.gambimparada@altran.com>
2019-12-09 11:38:35 +0000

    Initial Commit

---
+++ CoursesModelView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoursesModelView
+//
+//  Created by Abilio Gambim Parada on 09/12/2019.
+//  Copyright © 2019 Abilio Gambim Parada. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
